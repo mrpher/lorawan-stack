c4dfc51f48553b5b9af247a74a0de7237fcc5461
Bogdan <bogdans.afonins@thethingsindustries.com>
2021-02-18 10:02:07 +0200

    fixup! dev: Adjust e2e tests

--- cypress/integration/console/devices/create-repository.spec.js
+++ cypress/integration/console/devices/create-repository.spec.js
@@ -155,6 +155,33 @@ describe('End device repository create', () => {
         cy.findByTestId('full-error-view').should('not.exist')
       })
 
+      it('validates before submitting an empty form', () => {
+        cy.findByLabelText('Brand').selectOption('test-brand-otaa')
+        cy.findByLabelText('Model').selectOption('test-model2')
+
+        cy.findByRole('button', { name: 'Register end device' }).click()
+
+        cy.location('pathname').should(
+          'eq',
+          `${Cypress.config('consoleRootPath')}/applications/${appId}/devices/add/repository`,
+        )
+        cy.findErrorByLabelText('Frequency plan')
+          .should('contain.text', 'Frequency plan is required')
+          .and('be.visible')
+        cy.findErrorByLabelText('AppEUI')
+          .should('contain.text', 'AppEUI is required')
+          .and('be.visible')
+        cy.findErrorByLabelText('DevEUI')
+          .should('contain.text', 'DevEUI is required')
+          .and('be.visible')
+        cy.findErrorByLabelText('AppKey')
+          .should('contain.text', 'AppKey is required')
+          .and('be.visible')
+        cy.findErrorByLabelText('End device ID')
+          .should('contain.text', 'End device ID is required')
+          .and('be.visible')
+      })
+
       it('succeeds registering device', () => {
         const devId = 'test-model3-device'
 
@@ -386,6 +413,34 @@ describe('End device repository create', () => {
           .and('not.be.disabled')
       })
 
+      it('validates before submitting an empty form', () => {
+        cy.findByLabelText('Brand').selectOption('test-brand-abp')
+        cy.findByLabelText('Model').selectOption('test-model1')
+        cy.findByLabelText('Profile (Region)').selectOption('EU_863_870')
+
+        cy.findByRole('button', { name: 'Register end device' }).click()
+
+        cy.location('pathname').should(
+          'eq',
+          `${Cypress.config('consoleRootPath')}/applications/${appId}/devices/add/repository`,
+        )
+        cy.findErrorByLabelText('Frequency plan')
+          .should('contain.text', 'Frequency plan is required')
+          .and('be.visible')
+        cy.findErrorByLabelText('Device address')
+          .should('contain.text', 'Device address is required')
+          .and('be.visible')
+        cy.findErrorByLabelText('AppSKey')
+          .should('contain.text', 'AppSKey is required')
+          .and('be.visible')
+        cy.findErrorByLabelText('NwkSKey')
+          .should('contain.text', 'NwkSKey is required')
+          .and('be.visible')
+        cy.findErrorByLabelText('End device ID')
+          .should('contain.text', 'End device ID is required')
+          .and('be.visible')
+      })
+
       it('succeeds registering device', () => {
         const devId = 'test-abp-dev'
 
@@ -410,7 +465,7 @@ describe('End device repository create', () => {
         cy.findByTestId('full-error-view').should('not.exist')
       })
 
-      it.only('succeeds registering two the things uno', () => {
+      it('succeeds registering two devices', () => {
         const devId1 = 'test-abp-dev-1'
 
         // End device selection.
